05daaadfbad7c181596dc07ab4533fb3a6f52ad5
ozdemir <ozdemir@5736e3ea-d426-0410-8aaf-93e336ac3372>
2007-11-07 10:34:41 +0000

    services.py is cleaned from debug messages

--- service.py
+++ service.py
@@ -52,6 +52,7 @@ class Service(TranslationNode):
 		start_scan = None
 
 		sched = vex['SCHED']
+		chunk_number = 0
 		for scan in sched:
 			if not start_scan:
 				start_scan = scan
@@ -89,28 +90,16 @@ class Service(TranslationNode):
 			  chunk_size=(scan_end - scan_start)*(data_rate/8)
 
 			chunk_to_data_rate = int(chunk_size / (data_rate/8))
-			print "chunk_to_data_rate = ",chunk_to_data_rate
 			if chunk_to_data_rate == 0:
 				chunk_to_data_rate += 1
 				
-			print "chunk_to_data_rate = ",chunk_to_data_rate
-			
 			chunk_size = chunk_to_data_rate * (data_rate/8)
 			chunk_bytes = chunk_size
 
-			print "chunk size -> ", chunk_size
-
-			print "bits per sample ==> ", bits_per_sample
-			print "number of channels ==> ",num_channels
-			print "sample rate ==> ", sample_rate
-			print "data rate ==> ", data_rate 
 			start_position = float(vex.get_data_start(scan, station))
-			print "data starting position --> ", start_position
 			start_position = int(start_position*1000*1000*1000)
-			print "data starting position --> ", start_position
 			mark5_set_output = Mark5_set_position()
 			time_real_start = mark5_set_output.bytes_starting_position(portMark5Data, portMark5Control, ipMark, start_position)
-			print "time real start ==> ", time_real_start
 			time_real_start_ms = re.search("\.(\d{4})s",time_real_start)
 			time_real_start_ms = float("0."+time_real_start_ms.group(1))
 			time_real_start=re.sub("\.\d*","", time_real_start)
@@ -120,88 +109,41 @@ class Service(TranslationNode):
 			else:
 				time_start_diff = time_real_start-scan_start
 			bytes_starting_diff = (time_start_diff+time_real_start_ms)*(data_rate/8)
-			print "time real start ==> ", time_real_start
-			print "time_start_diff -> ", time_start_diff
-			print "time_float -> ", time_real_start_ms
-			print "bytes_starting_diff -> ", bytes_starting_diff
-			print "bytes_starting_diff / chunk size -> ", bytes_starting_diff / chunk_size
-
-			print "chunk_size / data_rate = ", chunk_size / (data_rate / 8)
 
 # Chop it up and chunks 
 			chunk_end = chunk_time + chunk_size / (data_rate / 8)
 			chunk_end = min(chunk_end, job_end)
 			chunk_start_check = max(job_start, scan_start)
 			
-			print "chunk start check--->", chunk_start_check
-			
 			chunk_real_end_size_temp = (scan_end-scan_start) * (data_rate / 8) / chunk_size
 			chunk_real_end_size = (chunk_real_end_size_temp - int(chunk_real_end_size_temp))*chunk_size
 			if chunk_real_end_size ==0:
 				chunk_real_end_size = chunk_size
-			print " <==================================================> " 
-			print " ===> scan_end", scan_end
-			print " ===> scan_start", scan_start
-			print " ===> chunk_real_end_size_temp", chunk_real_end_size_temp
-			print "chunk_real_end_size ===> ", chunk_real_end_size
-			print "chunk start--->", chunk_start
-			print "chunk end--->", chunk_end
-			print " ===> scan_end - chunk_end", scan_end-chunk_end
 			scan_size = (scan_end-scan_start)*(data_rate/8)
-			print "chunk_size - scan_size", chunk_size - scan_size
-			print " <==================================================> "
 			
-			chunk_number = 0
 			while chunk_end <= scan_end:
-				print "chunk", chunk_number, start_scan, \
+				print "chunk", chunk_number, scan, \
 					strftime("%Yy%jd%Hh%Mm%Ss", localtime(chunk_start)), scan, \
 					strftime("%Yy%jd%Hh%Mm%Ss", localtime(chunk_end))
 					
 				if (time_real_start > chunk_start_check and int(bytes_starting_diff/chunk_size)>0):
 					chunk_real_size=0
-					print "========> condition 1"
-					print "TIME REAL START in WHILE ==> ", time_real_start
-					print "CHUNK START CHECK in WHILE ==> ", chunk_start_check
-					print "CHUNK START CHECK - TIME REAL START ==> ", chunk_start_check - time_real_start
 				elif (time_real_start >= chunk_start_check and int(bytes_starting_diff/chunk_size)==0):
 					chunk_real_size=chunk_size - time_real_start_ms * (data_rate / 8)
-					print "========> condition 2"
-					print "TIME REAL START in WHILE ==> ", time_real_start
-					print "CHUNK START CHECK in WHILE ==> ", chunk_start_check
-					print "CHUNK START CHECK - TIME REAL START ==> ", chunk_start_check - time_real_start
 				elif (chunk_end == scan_end):
 					chunk_real_size = chunk_real_end_size
-					print "========> condition 3"
-					print "TIME REAL START in WHILE ==> ", time_real_start
-					print "CHUNK START CHECK in WHILE ==> ", chunk_start_check
-					print "CHUNK START CHECK - TIME REAL START ==> ", chunk_start_check - time_real_start
 				elif ((chunk_end-chunk_start)*(data_rate/8) < chunk_size):
 					chunk_real_size=(chunk_end-chunk_start)*(data_rate/8)
 				else :
 					chunk_real_size=chunk_size
-					print "========> condition 4"
-					print "TIME REAL START in WHILE ==> ", time_real_start
-					print "CHUNK START CHECK in WHILE ==> ", chunk_start_check
-					print "CHUNK START CHECK - TIME REAL START ==> ", chunk_start_check - time_real_start
 
-				print "chunk_real_size--->", chunk_real_size
-				print "chunk_real_real_real_size--->", (chunk_end-chunk_start)*(data_rate/8)
-
-				print "chunk start--->", chunk_start
-				print "chunk end--->", chunk_end
-				print "chunk time--->", chunk_time
 				chunk_diff=chunk_end-chunk_start
 				chunk_diff_size = chunk_diff*(data_rate/8)
-				print "chunk_diff---->", chunk_diff
-				print "chunk_diff_size---->", chunk_diff_size
-				print "job_end--->", job_end
-				print "scan_end--->", scan_end
 
 				chunk_start_check=chunk_end
 
-				sendFile = experiment_name[0] + '_' + station + '_' + str(start_scan) + "_" + str(chunk_number) + '.m5a'
+				sendFile = experiment_name[0] + '_' + station + '_' + str(scan) + "_" + str(chunk_number) + '.m5a'
 				sendFile = fileName + sendFile.lower()
-				print "File Name is =======================>", sendFile
 				mark5_chunk_output = Mark5_get_chunks(portMark5Data, portMark5Control, ipMark, sendFile, block_size, chunk_real_size, start_position)
 				print mark5_chunk_output
 				
@@ -220,8 +162,6 @@ class Service(TranslationNode):
 				elif scan_end >= job_end:
 					chunk_end = min(chunk_end, job_end)
 
-
-				print "chunk end is --> ", chunk_end
 				chunk_bytes = chunk_size
 				chunk_number += 1
 				start_scan = scan
@@ -233,6 +173,7 @@ class Service(TranslationNode):
 			chunk_number += 1
 			start_scan = scan
 			chunk_bytes -= (scan_end - scan_start) * (data_rate / 8)
+			chunk_number = 0
 			continue
 
 
