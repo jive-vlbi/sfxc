c9df6e49063dc666a0c021bc06048b7ee8536240
marchal <marchal@5736e3ea-d426-0410-8aaf-93e336ac3372>
2008-02-27 08:29:13 +0000

    Should fixed the bug introduced. Now it should work like before + having the command lines.

--- src/test_sfxc.py
+++ src/test_sfxc.py
@@ -18,12 +18,13 @@ def usage():
   print "  --np d: number of processes"
 
 try:
-  opts, args = getopt.getopt(sys.argv[1:], "hn:x", ["help", "np=", "ctrl="])
+  opts, args = getopt.getopt(sys.argv[1:], "hn:x", ["help", "np=", "ctrl=", "vex="])
 except getopt.GetoptError:
   usage()
   sys.exit(2)
 
 controlfiles = []
+controlfile = None
 for opt, arg in opts:
   if opt in ("-h", "--help"):
     usage()
@@ -31,7 +32,13 @@ for opt, arg in opts:
   if opt in ("-n", "--np"):
     numProcesses = arg;
   if opt in ("--ctrl"):
-    controlfiles.append(arg);
+   controlfile = arg;
+  if opt in ("--vex"):
+    vexfile = arg;
+
+if not controlfile == None:
+  controlfiles = [[controlfile, vexfile]]
+
 
 # Load the ccf files for testing:
 RC_FILE = os.path.join(os.environ.get('HOME'), ".sfxcrc")
@@ -44,7 +51,7 @@ if (status != 0): sys.exit(1)
 
 # run the executable on all ccf files
 for ctrlfile in controlfiles:
-  cmd = "mpirun -np "+str(numProcesses)+" sfxc "+" "+ctrlfile
+  cmd = "mpirun -np "+str(numProcesses)+" sfxc "+" ".join(ctrlfile)
   print cmd
   status = os.system(cmd)
   if (status != 0): sys.exit(1)
