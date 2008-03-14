da5c5a7574cdcecb077be1cca02231ee16b1e33f
ozdemir <ozdemir@5736e3ea-d426-0410-8aaf-93e336ac3372>
2008-03-14 09:03:38 +0000

    Notification simulator works now as expected.

--- gridBrokerRequestSimulator.py
+++ gridBrokerRequestSimulator.py
@@ -1,27 +1,20 @@
+import sys
 from TranslationNodeNotification_services import *
 
 # define request
 req = chunkIsReadyRequest()
-req.Param0 = req.new_param0()  
+req.Param0 = req.new_param0()
 
 # get broker ip address from the read file
-req.Param0.BrokerIPAddress="192.42.120.69"
-req.Param0.chunkId = 0
-req.Param0.chunkLocation = "huygens/data4/"
-req.Param0.chunkSize = 123456
-req.Param0.endTime = "2007y158d18h59m00s"
-req.Param0.startTime = "2007y158d18h56m00s"
-req.Param0.translationNodeIP = "192.42.120.22"
-req.Param0.translationNodeId = "huygens"
-
-
-print 'chunk ID: ', req.Param0.chunkId
-print 'chunk Location: ', req.Param0.chunkLocation
-print 'Requested chunk size: ', req.Param0.chunkSize
-print 'Requested end time: ', req.Param0.endTime
-print 'Requested start time: ', req.Param0.startTime
-print 'translationNode IP: ', req.Param0.translationNodeIP
-print 'translationNode Id: ', req.Param0.translationNodeId
+# actually both declaration types work
+# declaring i.e. req.chunkId=0 wouldn't work
+req.Param0.ChunkId = 10001
+req.Param0.set_element_chunkLocation("huygens/data4/")
+req.Param0.set_element_chunkSize(123456)
+req.Param0.set_element_endTime("2007y158d18h59m00s")
+req.Param0.set_element_startTime("2007y158d18h56m00s")
+req.Param0.set_element_translationNodeIP("192.42.120.22")
+req.Param0.set_element_translationNodeId(20001)
 
 # get port number from the read file
 portNumber_string = "8080"
@@ -29,10 +22,15 @@ portNumber = portNumber_string.strip()
 print portNumber
 
 # test if the http address correctly parsed
+# get a port proxy instance
 loc = TranslationNodeNotificationLocator()
 portTest = 'http://localhost:' + portNumber + '/test'
 print portTest
-port = loc.getTranslationNodeNotificationPortType(portTest)
+port = loc.getTranslationNodeNotificationPortType(portTest, tracefile=sys.stdout)
+
+# Note that both of the following implementations work
+print "Chunk Id", req.Param0.get_element_chunkId()
+print "Chunk Location", req.Param0.ChunkLocation
 
 # actualy ask the service to do the job
 resp = port.chunkIsReady(req)
--- gridBrokerServiceSimulator.py
+++ gridBrokerServiceSimulator.py
@@ -5,20 +5,20 @@ from TranslationNodeNotification_services_server import *
 
 class Service(TranslationNodeNotification):
   def soap_chunkIsReady(self, ps):
-    rsp = TranslationNodeNotification.soap_chunkIsReady(self, ps)    
+    rsp = TranslationNodeNotification.soap_chunkIsReady(self, ps)
     msg = self.request
 
     print "hello Mark..."
-    print 'chunk ID: ', msg.Param0._chunkId
-    print 'chunk Location: ', msg.Param0._chunkLocation
-#    print 'Requested chunk size: ', msg.Param0.chunkSize
-#    print 'Requested end time: ', msg.Param0.endTime
-#    print 'Requested start time: ', msg.Param0.startTime
-#    print 'translationNode IP: ', msg.Param0.translationNodeIP
-#    print 'translationNode Id: ', msg.Param0.translationNodeId
+    print 'chunk ID: ', msg.Param0.get_element_chunkId()
+    print 'chunk Location: ', msg.Param0.get_element_chunkLocation()
+    print 'Requested chunk size: ', msg.Param0.get_element_chunkSize()
+    print 'Requested end time: ', msg.Param0.get_element_endTime()
+    print 'Requested start time: ', msg.Param0.get_element_startTime()
+    print 'translationNode IP: ', msg.Param0.get_element_translationNodeIP()
+    print 'translationNode Id: ', msg.Param0.get_element_translationNodeId()
         
     return rsp
 
 if __name__ == "__main__" :
-	port = 8080
-	AsServer(port, (Service('test'),))
+  port = 8080
+  AsServer(port, (Service('test'),))
