68fe60c20bc88ee1cab84fb8ec4629dc639c7085
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2010-12-14 13:30:33 +0000

    Turn this into a proper daemon.

--- mk5read/mk5read.c
+++ mk5read/mk5read.c
@@ -1,10 +1,30 @@
+/*
+ * Copyright (c) 2003, 2004 Henning Brauer <henning@openbsd.org>
+ * Copyright (c) 2010 Joint Institute for VLBI in Europe
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <errno.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -20,12 +40,60 @@
 #define min(a,b) ((a) < (b) ? (a) : (b))
 
 volatile sig_atomic_t die;
+int debug;
+
+void	logit(int, const char *, ...);
+void	vlog(int, const char *, va_list);
 
 void
-fatal(const char *s)
+log_init(int n_debug)
 {
-	perror(s);
-	exit(EXIT_FAILURE);
+	extern char *__progname;
+
+	debug = n_debug;
+
+	if (!debug)
+		openlog(__progname, LOG_PID | LOG_NDELAY, LOG_DAEMON);
+
+	tzset();
+}
+
+void
+fatal(const char *emsg)
+{
+	if (errno)
+		logit(LOG_CRIT, "fatal: %s: %s\n", emsg, strerror(errno));
+	else
+		logit(LOG_CRIT, "fatal: %s\n", emsg);
+}
+
+void
+logit(int pri, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vlog(pri, fmt, ap);
+	va_end(ap);
+}
+
+void
+vlog(int pri, const char *fmt, va_list ap)
+{
+	char *nfmt;
+
+	if (debug) {
+		/* best effort in out of mem situations */
+		if (asprintf(&nfmt, "%s\n", fmt) == -1) {
+			vfprintf(stderr, fmt, ap);
+			fprintf(stderr, "\n");
+		} else {
+			vfprintf(stderr, nfmt, ap);
+			free(nfmt);
+		}
+		fflush(stderr);
+	} else
+		vsyslog(pri, fmt, ap);
 }
 
 int64_t
@@ -56,7 +124,7 @@ stream_data(SSHANDLE xlrHandle, uint64_t off, int fd)
 		if (XLRRead(xlrHandle, &readDesc) != XLR_SUCCESS) {
 			xlrError = XLRGetLastError();
 			XLRGetErrorMessage(errString, xlrError);
-			printf("%s (%d)\n", errString, (int)xlrError);
+			logit(LOG_CRIT, "%s", errString);
 			break;
 		}
 
@@ -73,40 +141,68 @@ stream_data(SSHANDLE xlrHandle, uint64_t off, int fd)
 int
 open_diskpack(const char *vsn, SSHANDLE *xlrHandle)
 {
+	ULONG xlrError;
+	char errString[XLR_ERROR_LENGTH];
 	S_BANKSTATUS bankStatus;
 	UINT bankID;
 	char *p;
 
-	if (XLROpen(1, xlrHandle) != XLR_SUCCESS)
+	if (XLROpen(1, xlrHandle) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
+		XLRClose(*xlrHandle);
 		return -1;
+	}
 
 	if (XLRSetMode(*xlrHandle, SS_MODE_SINGLE_CHANNEL) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
 
 	if (XLRClearChannels(*xlrHandle) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
 
 	if (XLRSelectChannel(*xlrHandle, 0) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
 
 	if (XLRBindOutputChannel(*xlrHandle, 0) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
 
 	if (XLRSetBankMode(*xlrHandle, SS_BANKMODE_NORMAL) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
 
+	/*
+	 * 
+	 */
 	for (bankID = BANK_A; bankID != BANK_INVALID; bankID++) {
 		if (XLRGetBankStatus(*xlrHandle, bankID, &bankStatus) != XLR_SUCCESS) {
+			xlrError = XLRGetLastError();
+			XLRGetErrorMessage(errString, xlrError);
+			logit(LOG_CRIT, "%s", errString);
 			bankID = BANK_INVALID;
 			break;
 		}
@@ -124,6 +220,9 @@ open_diskpack(const char *vsn, SSHANDLE *xlrHandle)
 		return -1;
 
 	if (XLRSelectBank(*xlrHandle, bankID) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
 		XLRClose(*xlrHandle);
 		return -1;
 	}
@@ -137,20 +236,51 @@ handler(int sig)
 	die = 1;
 }
 
+void
+usage(void)
+{
+	extern char *__progname;
+
+	fprintf(stderr, "usage: %s [-d]\n", __progname);
+	exit(EXIT_FAILURE);
+}
+
 int
-main(void)
+main(int argc, char *argv[])
 {
  	SSHANDLE xlrHandle;
-	ULONG xlrError;
-	char errString[XLR_ERROR_LENGTH];
-
 	struct sockaddr_un sun;
 	socklen_t len;
 	int s, fd;
-
 	struct mk5read_msg msg;
 	time_t t1, t2;
 	int64_t xlen;
+	int debug = 0;
+	int ch;
+
+	log_init(1);
+
+	while ((ch = getopt(argc, argv, "d")) != -1) {
+		switch (ch) {
+		case 'd':
+			debug = 1;
+			break;
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+	if (argc > 0)
+		usage();
+
+	if (!debug)
+		if (daemon(0, 0))
+			fatal("daemon");
+
+	log_init(debug);
 
 	signal(SIGTERM, handler);
 	signal(SIGPIPE, SIG_IGN);
@@ -177,12 +307,11 @@ main(void)
 		if (read(fd, &msg, sizeof(msg)) != sizeof(msg))
 			fatal("read");
 
-		printf("%s:%llu\n", msg.vsn, (unsigned long long)msg.off);
+		if (debug)
+			printf("%s:%llu\n", msg.vsn,
+			    (unsigned long long)msg.off);
 
 		if (open_diskpack(msg.vsn, &xlrHandle) == -1) {
-			xlrError = XLRGetLastError();
-			XLRGetErrorMessage(errString, xlrError);
-			printf("%s\n", errString);
 			close(fd);
 			continue;
 		}
@@ -190,7 +319,7 @@ main(void)
 		t1 = time(NULL);
 		xlen = stream_data(xlrHandle, msg.off, fd);
 		t2 = time(NULL);
-		if (xlen != -1 && t1 != t2)
+		if (debug && xlen != -1 && t1 != t2)
 			printf("%lld bytes in %d seconds: %f Mbit/s\n",
 			       (long long)xlen, (int)(t2 - t1),
 			       (double)xlen * 8 * 1e-6 / (t2 - t1));
