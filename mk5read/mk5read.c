8a59d6d092c1816dc1711b95d7a5fe7759121353
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2012-04-03 13:51:53 +0000

    Add support for a bank_set? Mark5A compatibility command. Add Mark5C support.

--- mk5read/mk5read.c
+++ mk5read/mk5read.c
@@ -17,8 +17,14 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <ctype.h>
 #include <errno.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -30,6 +36,12 @@
 
 #include "xlrapi.h"
 
+/* Backwards compatibility for SDK 8. */
+#ifndef XLR_MAX_IP_ADDR
+#define UINT32 UINT
+#define PUINT32 PULONG
+#endif
+
 #include "mk5read.h"
 
 #define MK5READ_SIZE	(256 * 4096)
@@ -40,6 +52,11 @@
 #define min(a,b) ((a) < (b) ? (a) : (b))
 
 int debug;
+
+pthread_t command_thread;
+pthread_mutex_t ss_lock;
+
+char Label[BANK_INVALID][XLR_LABEL_LENGTH];
 
 void	logit(int, const char *, ...);
 void	vlog(int, const char *, va_list);
@@ -95,13 +112,56 @@ vlog(int pri, const char *fmt, va_list ap)
 		vsyslog(pri, fmt, ap);
 }
 
+void
+read_labels(void)
+{
+	SSHANDLE xlrHandle;
+	ULONG xlrError;
+	char errString[XLR_ERROR_LENGTH];
+	S_BANKSTATUS bankStatus;
+	UINT32 bankID;
+
+	if (XLROpen(1, &xlrHandle) != XLR_SUCCESS) {
+		xlrError = XLRGetLastError();
+		XLRGetErrorMessage(errString, xlrError);
+		logit(LOG_CRIT, "%s", errString);
+		XLRClose(xlrHandle);
+		memset(Label, 0, sizeof(Label));
+		return;
+	}
+
+	for (bankID = BANK_A; bankID != BANK_INVALID; bankID++) {
+		if (XLRGetBankStatus(xlrHandle, bankID, &bankStatus) != XLR_SUCCESS) {
+			xlrError = XLRGetLastError();
+			XLRGetErrorMessage(errString, xlrError);
+			logit(LOG_CRIT, "%s", errString);
+			memset(Label[bankID], 0, XLR_LABEL_LENGTH);
+			continue;
+		}
+
+		if (bankStatus.State == STATE_READY) {
+			for (size_t i = 0; i < XLR_LABEL_LENGTH; i++) {
+				if (!isprint(bankStatus.Label[i])) {
+					bankStatus.Label[i] = 0;
+					break;
+				}
+			}
+			strlcpy(Label[bankID], bankStatus.Label,
+			    XLR_LABEL_LENGTH);
+		} else
+			memset(Label[bankID], 0, XLR_LABEL_LENGTH);
+	}
+
+	XLRClose(xlrHandle);
+}
+
 int64_t
 stream_data(SSHANDLE xlrHandle, uint64_t off, int fd)
 {
 	S_READDESC readDesc;
 	ULONG xlrError;
 	char errString[XLR_ERROR_LENGTH];
-	ULONG *buf;
+	void *buf;
 	DWORDLONG recordingLength;
 	uint64_t len = 0, size;
 	ssize_t nbytes;
@@ -114,7 +174,7 @@ stream_data(SSHANDLE xlrHandle, uint64_t off, int fd)
 		return -1;
 	}
 
-	buf = (ULONG *)malloc(MK5READ_SIZE);
+	buf = malloc(MK5READ_SIZE);
 	if (buf == NULL)
 		return -1;
 
@@ -125,7 +185,7 @@ stream_data(SSHANDLE xlrHandle, uint64_t off, int fd)
 		readDesc.AddrHi = off >> 32;
 		readDesc.AddrLo = off & 0xffffffff;
 		readDesc.XferLength = size;
-		readDesc.BufferAddr = buf;
+		readDesc.BufferAddr = (PUINT32)buf;
 		if (XLRRead(xlrHandle, &readDesc) != XLR_SUCCESS) {
 			xlrError = XLRGetLastError();
 			XLRGetErrorMessage(errString, xlrError);
@@ -166,7 +226,7 @@ open_diskpack(const char *vsn, SSHANDLE *xlrHandle)
 	ULONG xlrError;
 	char errString[XLR_ERROR_LENGTH];
 	S_BANKSTATUS bankStatus;
-	UINT bankID;
+	UINT32 bankID, matchedBankID;
 	char *p;
 
 	if (XLROpen(1, xlrHandle) != XLR_SUCCESS) {
@@ -225,28 +285,35 @@ open_diskpack(const char *vsn, SSHANDLE *xlrHandle)
 		return -1;
 	}
 
-	/*
-	 * 
-	 */
+	matchedBankID = BANK_INVALID;
 	for (bankID = BANK_A; bankID != BANK_INVALID; bankID++) {
 		if (XLRGetBankStatus(*xlrHandle, bankID, &bankStatus) != XLR_SUCCESS) {
 			xlrError = XLRGetLastError();
 			XLRGetErrorMessage(errString, xlrError);
 			logit(LOG_CRIT, "%s", errString);
-			bankID = BANK_INVALID;
-			break;
+			memset(Label[bankID], 0, XLR_LABEL_LENGTH);
+			continue;
 		}
 
 		if (bankStatus.State == STATE_READY) {
+			for (size_t i = 0; i < XLR_LABEL_LENGTH; i++) {
+				if (!isprint(bankStatus.Label[i])) {
+					bankStatus.Label[i] = 0;
+					break;
+				}
+			}
+			strlcpy(Label[bankID], bankStatus.Label,
+			    XLR_LABEL_LENGTH);
 			p = strchr(bankStatus.Label, '/');
 			if (p)
 				*p = 0;
 			if (strcmp(vsn, bankStatus.Label) == 0)
-				break;
-		}
+				matchedBankID = bankID;
+		} else
+			memset(Label[bankID], 0, XLR_LABEL_LENGTH);
 	}
 
-	if (bankID == BANK_INVALID)
+	if (matchedBankID == BANK_INVALID)
 		return -1;
 
 	if (bankStatus.MediaStatus == MEDIASTATUS_FAULTED) {
@@ -259,7 +326,7 @@ open_diskpack(const char *vsn, SSHANDLE *xlrHandle)
 		}
 	}
 
-	if (XLRSelectBank(*xlrHandle, bankID) != XLR_SUCCESS) {
+	if (XLRSelectBank(*xlrHandle, matchedBankID) != XLR_SUCCESS) {
 		xlrError = XLRGetLastError();
 		XLRGetErrorMessage(errString, xlrError);
 		logit(LOG_CRIT, "%s", errString);
@@ -286,6 +353,86 @@ usage(void)
 	exit(EXIT_FAILURE);
 }
 
+void *
+command_loop(void *arg)
+{
+	char request[1024];
+	char reply[1024];
+	struct sockaddr_in sin;
+	socklen_t len;
+	ssize_t nbytes;
+	int s, fd;
+
+	s = socket(PF_INET, SOCK_STREAM, 0);
+	if (s == -1) {
+		logit(LOG_CRIT, "socket: %s", strerror(errno));
+		return NULL;
+	}
+
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(2620);
+	sin.sin_addr.s_addr = INADDR_ANY;
+
+	while (bind(s, (struct sockaddr *)&sin, sizeof(sin)) == -1) {
+		if (errno == EADDRINUSE) {
+			sleep(10);
+			continue;
+		}
+		logit(LOG_CRIT, "bind: %s", strerror(errno));
+		close(s);
+		return NULL;
+	}
+
+	if (listen(s, 16) == -1) {
+		logit(LOG_CRIT, "listen: %s", strerror(errno));
+		close(s);
+		return NULL;
+	}
+
+	for (;;) {
+		len = sizeof(sin);
+		fd = accept(s, (struct sockaddr *)&sin, &len);
+		if (fd == -1) {
+			fprintf(stderr, "xxx\n");
+			logit(LOG_CRIT, "accept: %s", strerror(errno));
+			close(s);
+			return NULL;
+		}
+
+		for (;;) {
+			nbytes = read(fd, request, sizeof(request));
+			if (nbytes <= 0)
+				break;
+
+			request[sizeof(request) - 1] = 0;
+			if (strncmp(request, "bank_set?", strlen("bank_set?")) == 0) {
+				if (pthread_mutex_trylock(&ss_lock) == 0) {
+					read_labels();
+					pthread_mutex_unlock(&ss_lock);
+				}
+
+				snprintf(reply, sizeof(reply), 
+				    "!bank_set? 0 : A : %s : B : %s ; \n",
+				    Label[BANK_A], Label[BANK_B]);
+			} else {
+				for (size_t i = 0; i < strlen(request); i++) {
+					if (isspace(request[i])) {
+						request[i] = 0;
+					}
+				}
+				snprintf(reply, sizeof(reply),
+				    "!%s = 7 : Unknown keyword ; \n", request);
+			}
+			write(fd, reply, strlen(reply));
+		}
+
+		close(fd);
+	}
+
+	close(s);
+	return NULL;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -297,6 +444,7 @@ main(int argc, char *argv[])
 	time_t t1, t2;
 	int64_t xlen;
 	int debug = 0;
+	int err;
 	int ch;
 
 	log_init(1);
@@ -323,6 +471,8 @@ main(int argc, char *argv[])
 
 	log_init(debug);
 
+	signal(SIGINT, handler);
+	signal(SIGHUP, handler);
 	signal(SIGTERM, handler);
 	signal(SIGPIPE, SIG_IGN);
 
@@ -335,9 +485,16 @@ main(int argc, char *argv[])
 	unlink(MK5READ_SOCKET);
 	if (bind(s, (struct sockaddr *)&sun, sizeof(sun)) == -1)
 		fatal("bind");
+	chmod(MK5READ_SOCKET, 0666);
 
 	if (listen(s, 1) == -1)
 		fatal("listen");
+
+	err = pthread_mutex_init(&ss_lock, NULL);
+	assert(err == 0);
+
+	err = pthread_create(&command_thread, NULL, command_loop, NULL);
+	assert(err == 0);
 
 	for (;;) {
 		len = sizeof(sun);
@@ -351,6 +508,8 @@ main(int argc, char *argv[])
 		if (debug)
 			printf("%s:%llu\n", msg.vsn,
 			    (unsigned long long)msg.off);
+
+		pthread_mutex_lock(&ss_lock);
 
 		if (open_diskpack(msg.vsn, &xlrHandle) == -1) {
 			close(fd);
@@ -366,6 +525,8 @@ main(int argc, char *argv[])
 			       (double)xlen * 8 * 1e-6 / (t2 - t1));
 
 		XLRClose(xlrHandle);
+
+		pthread_mutex_unlock(&ss_lock);
 
 		close(fd);
 	}
