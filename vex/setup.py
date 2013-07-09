ed06bf0812ffdfb39ddc9678cf586456b16dc513
kettenis <kettenis@5736e3ea-d426-0410-8aaf-93e336ac3372>
2013-07-09 16:15:23 +0000

    Add documentatuon and license.

--- vex/setup.py
+++ vex/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 
-setup(name='vex', version='1.0', description='VEX Parser',
+setup(name='vex', version='1.1', description='VEX Parser',
       author='Mark Kettenis', author_email='kettenis@jive.nl',
       py_modules=['vex', 'MultiDict'])
